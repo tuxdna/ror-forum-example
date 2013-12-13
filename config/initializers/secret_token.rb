f8ca36e84c879b0e23bec3b5f65a2c5b4659039c
Saleem Ansari <tuxdna@gmail.com>
2013-12-13 21:06:40 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = '0e75e5709f75fbba2dc53e5b2860e33da28647c4025555383bf5c5f5b5eda1ee107e872b7d66d1e7b2c5a1c1a921ac87b759b7e136a079e8f869f75296f00308'
